523c226ab5ececa883a12933ba11a2a50fb8618b
NastyaSimakova <simakovaa47@gmail.com>
2025-02-28 14:46:14 +0200

    Восстановлен Lab.iml

--- src/Task3.java
+++
@@ -1,9 +0,0 @@
-
-public class Task3 {
-    public static void main(String[] args) {
-        byte num1;
-        num1 = 120;
-        double num2 = num1 - 20;
-        System.out.println(num2);
-    }
-}
